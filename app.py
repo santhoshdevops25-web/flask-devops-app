c8578523f25f71cd61a23074e07dcca871e15565
santhoshdevops25 <santhoshdevops25@gmail.com>
2026-07-20 03:26:22 +0800

    changed

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "!!version 2 - webhook working!--by Santhosh"
+    return "version 2 - webhook working!--by Santhosh"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
