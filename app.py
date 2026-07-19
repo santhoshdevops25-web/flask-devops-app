4ac3989502058f7dd68ce0c372f27cd0f9f9a09f
santhoshdevops25 <santhoshdevops25@gmail.com>
2026-07-20 03:08:10 +0800

    Test github webhook

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello from Jenkins CI/CD on AWS!"
+    return "version 2 - webhook working!"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
